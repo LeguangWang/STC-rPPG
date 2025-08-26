3452ec99144723ca30b92f5a7da37eedefea8cd9
LeguangWang <wanglg@mail.tust.edu.cn>
2025-08-26 11:47:35 +0800

    Add files via upload

---
+++ dataset/Rnet.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
+
+class RnetDataset(Dataset):
+    def __init__(self, video_data, label_data,label_HR):
+        self.transform = transforms.Compose([transforms.ToTensor()])
+        self.video_data = video_data
+        self.label = label_data
+        self.label_HR = label_HR
+
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+
+        video_data = torch.tensor(np.transpose(self.video_data[index], (3, 0, 1, 2)), dtype=torch.float32)
+        label_data = torch.tensor(self.label[index], dtype=torch.float32)
+        label_HR = torch.tensor(self.label_HR[index], dtype=torch.float32)
+
+        if torch.cuda.is_available():
+            video_data = video_data.to('cuda')
+            label_data = label_data.to('cuda')
+            label_HR = label_HR.to('cuda')
+        targets = torch.stack([label_data,label_HR],dim=0)
+        return video_data, targets
+
+    def __len__(self):
+        return len(self.label)
+
